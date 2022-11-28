b99caa1fa7a29a0d307a6c7aef15c5b8278c779a
Pavel Yarokhovich <pavel.yarokhovich@cactussoft.biz>
2022-11-28 21:09:12 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -131,7 +131,7 @@ function App() {
             <section className="App-content-create">
               <h3>Add teammate</h3>
               <Form
-                values={{}}
+                values={{ name: "", occupation: "" }}
                 onSubmit={createTeammate}
                 onCancel={() => setIsCreating(false)}
               />
